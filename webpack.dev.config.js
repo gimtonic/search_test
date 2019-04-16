d67a4c1d2eafb08949f54a4ae7174ad720d6229d
Антон Королев <a.korolev@ddemo.ru>
2019-04-16 22:23:48 +0700

    frontend add

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,10 +1,33 @@
 const path = require('path');
+const config = require('./config');
 
 module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist')
+    },
+    // cheap-module-eval-source-map is faster for development
+    devtool: config.dev.devtool,
+
+    // these devServer options should be customized in /config/index.js
+    devServer: {
+        clientLogLevel: 'warning',
+        historyApiFallback: true,
+        hot: true,
+        compress: true,
+        host: config.dev.host,
+        port: config.dev.port,
+        open: config.dev.autoOpenBrowser,
+        overlay: config.dev.errorOverlay
+            ? { warnings: false, errors: true }
+            : false,
+        publicPath: config.dev.assetsPublicPath,
+        proxy: config.dev.proxyTable,
+        quiet: true, // necessary for FriendlyErrorsPlugin
+        watchOptions: {
+            poll: config.dev.poll,
+        }
     },
     module: {
         rules: [
